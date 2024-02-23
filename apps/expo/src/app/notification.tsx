06018f292d397640ce1eeef1c903244f483463a9
raeperd <raeperd117@gmail.com>
2024-02-23 17:02:07 +0900

    refactor: /notification Simplified

--- apps/expo/src/app/notification.tsx
+++ apps/expo/src/app/notification.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import type { ViewProps } from "react-native";
+import React, { useState } from "react";
 import { Switch, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -6,6 +8,8 @@ import RouteBackButton from "../components/RouteBackButton";
 
 export default function Notification() {
   const [enabled, setEnabled] = useState(false);
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
   return (
     <SafeAreaView className="flex-1 bg-black px-4">
       <RouteBackButton>알림설정</RouteBackButton>
@@ -25,23 +29,51 @@ export default function Notification() {
             <Text
               className="mt-2.5 text-center font-bold text-white"
               style={{ fontSize: 40 }}
-            >{`00:00`}</Text>
+            >{`${hour.toString().padStart(2, "0")}:${minute
+              .toString()
+              .padStart(2, "0")}`}</Text>
           </View>
-          <View className="mt-[80] w-full items-center rounded-[14] bg-white py-[30]">
-            <Text className="font-medium">판매 마감시간 기준</Text>
-            <Text>시간 분 전</Text>
-            <Text className="font-medium">알림을 받을게요.</Text>
-          </View>
-          <View className="bg-gray_5 mt-3 w-full rounded-[14] py-[13] pl-[10]">
-            <Text className="text-sm text-[#bcbdc1]">
-              - 판매 마감 시간: 매주 토요일 오후 8시
-            </Text>
-            <Text className="text-sm text-[#bcbdc1]">
-              - 알림 시간 도달 시 알림이 발송됩니다.
-            </Text>
-          </View>
+          <NotificationSettingsView
+            className="mt-[80] w-full items-center rounded-[14] bg-white py-[30]"
+            hour={hour}
+            setHour={setHour}
+            minute={minute}
+            setMinute={setMinute}
+          ></NotificationSettingsView>
+          <NoticeView className="bg-gray_5 mt-3 w-full rounded-[14] py-[13] pl-[10]"></NoticeView>
         </View>
       </View>
     </SafeAreaView>
+  );
+}
+
+function NotificationSettingsView(props: NotificationSettingsProps) {
+  const { hour, setHour, minute, setMinute, ...rest } = props;
+  return (
+    <View className="absolute" {...rest}>
+      <Text className="font-medium">판매 마감시간 기준</Text>
+      <Text>시간 분 전</Text>
+      <Text className="font-medium">알림을 받을게요.</Text>
+    </View>
+  );
+}
+
+interface NotificationSettingsProps extends ViewProps {
+  hour: number;
+  setHour: Dispatch<SetStateAction<number>>;
+  minute: number;
+  setMinute: Dispatch<SetStateAction<number>>;
+}
+
+function NoticeView(props: ViewProps) {
+  return (
+    <View {...props}>
+      <Text className="text-sm text-[#bcbdc1]">
+        - 판매 마감 시간: 매주 토요일 오후 8시
+      </Text>
+      <Text className="text-sm text-[#bcbdc1]">
+        - 알림 시간 도달 시 알림이 발송됩니다.
+      </Text>
+    </View>
   );
 }
