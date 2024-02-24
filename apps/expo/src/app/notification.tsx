9ccaa6cfebd987dbda7ed81d8b465d7fcd810b47
raeperd <raeperd117@gmail.com>
2024-02-24 13:45:25 +0900

    feat: /notification Add design with states

--- apps/expo/src/app/notification.tsx
+++ apps/expo/src/app/notification.tsx
@@ -1,7 +1,8 @@
 import type { Dispatch, SetStateAction } from "react";
 import type { ViewProps } from "react-native";
 import React, { useState } from "react";
-import { Switch, Text, View } from "react-native";
+import { Pressable, Switch, Text, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import RouteBackButton from "../components/RouteBackButton";
@@ -34,7 +35,9 @@ export default function Notification() {
               .padStart(2, "0")}`}</Text>
           </View>
           <NotificationSettingsView
-            className="mt-[80] w-full items-center rounded-[14] bg-white py-[30]"
+            className={`${
+              enabled ? "" : "opacity-20"
+            } mt-[80] w-full items-center rounded-[14] bg-[#ededed] py-[30]`}
             hour={hour}
             setHour={setHour}
             minute={minute}
@@ -47,13 +50,107 @@ export default function Notification() {
   );
 }
 
-function NotificationSettingsView(props: NotificationSettingsProps) {
-  const { hour, setHour, minute, setMinute, ...rest } = props;
+function NotificationSettingsView({
+  hour,
+  setHour,
+  minute,
+  setMinute,
+  className,
+  ...rest
+}: NotificationSettingsProps) {
   return (
-    <View className="absolute" {...rest}>
+    <View className={`${className}`} {...rest}>
       <Text className="font-medium">판매 마감시간 기준</Text>
-      <Text>시간 분 전</Text>
+      <View className="flex-row items-center gap-1">
+        <DropdownComponent hour={hour} setHour={setHour}></DropdownComponent>
+        <Text>시간</Text>
+        <View></View>
+        <MinuteView minute={minute} setMinute={setMinute}></MinuteView>
+        <Text>분 전</Text>
+      </View>
       <Text className="font-medium">알림을 받을게요.</Text>
+    </View>
+  );
+}
+
+const DropdownComponent = ({
+  hour,
+  setHour,
+  ...rest
+}: ViewProps & {
+  hour: number;
+  setHour: Dispatch<SetStateAction<number>>;
+}) => {
+  const hours = Array.from({ length: 4 }, (_, i) => ({
+    label: i.toString(),
+    value: i.toString(),
+  }));
+
+  return (
+    <View className="w-[50] bg-[#ededed]" {...rest}>
+      <Dropdown
+        style={{
+          borderColor: "#6e2bfc",
+          backgroundColor: "#6E2bfc",
+          borderRadius: 8,
+        }}
+        selectedTextStyle={{ color: "white", textAlign: "center" }}
+        iconStyle={{ display: "none" }}
+        renderItem={(item, selected) => {
+          return (
+            <View
+              className={`${
+                selected ? "bg-point" : "bg-white"
+              } border-point flex justify-center rounded-[8] p-2`}
+              style={{ borderWidth: 1 }}
+            >
+              <Text
+                className={`${
+                  selected ? "text-white" : "text-point"
+                } text-center font-semibold`}
+              >
+                {item.value}
+              </Text>
+            </View>
+          );
+        }}
+        value={hour.toString()}
+        data={hours}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        onChange={(item) => setHour(parseInt(item.value, 10))}
+      />
+    </View>
+  );
+};
+
+function MinuteView(
+  props: ViewProps & {
+    minute: number;
+    setMinute: Dispatch<SetStateAction<number>>;
+  },
+) {
+  const { className, minute, setMinute, ...rest } = props;
+  return (
+    <View className={`flex-row bg-white ${className}`} {...rest}>
+      {[0, 30].map((m) => (
+        <Pressable
+          key={m}
+          onPress={() => setMinute(m)}
+          className={`${
+            m == minute ? "bg-point" : "bg-[#EDEDED]"
+          } rounded-[4] px-5 py-[4.5]`}
+        >
+          <Text
+            className={`${
+              minute == m ? "text-white" : "text-gray_4"
+            } text-xl font-medium`}
+          >
+            {m}
+          </Text>
+        </Pressable>
+      ))}
     </View>
   );
 }
